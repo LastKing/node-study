010f9c67b02f5485c887d6caf9fb007892f82519
李太新 <toonew@danwi.me>
2015-11-19 18:30:35 +0800

    上传基本登陆页面

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/login.do', (req, res, next)=> {
+
+  var userNaer = req.body.userName;
+  var password = req.body.password;
+
+  if (userNaer === 'admin' && password == '123123') {
+    res.send(JSON.stringify({result: 0}))
+  } else {
+    res.send(JSON.stringify({result: -1, reason: '账户或者密码错误！~！'}))
+  }
+
 });
 
 module.exports = router;
