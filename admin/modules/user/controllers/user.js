3d39dd0d59a3917eb53723b2bbe8685a940b0311
李太新 <toonew@danwi.me>
2015-11-20 17:46:26 +0800

    登陆界面修改，以及用户界面提交

--- admin/modules/user/controllers/user.js
+++ admin/modules/user/controllers/user.js
@@ -2,6 +2,13 @@
  * Created by Rain on 2015/11/20.
  */
 var UserCtrl = user.controller('UserCtrl', function ($scope) {
+  $scope.title = '用户页面';
 
-
+  $scope.users = [
+    {userName: '张三', age: '18', email: 'litaixin01@163.com'},
+    {userName: '张三', age: '18', email: 'litaixin01@163.com'},
+    {userName: '张三', age: '18', email: 'litaixin01@163.com'},
+    {userName: '张三', age: '18', email: 'litaixin01@163.com'},
+    {userName: '张三', age: '18', email: 'litaixin01@163.com'}
+  ]
 });
