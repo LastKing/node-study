73649d47c5f490478f9c15727a59af4e156f904c
李太新 <toonew@danwi.me>
2015-11-20 19:01:19 +0800

    登陆方法 路径修改

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,25 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/login.do', (req, res)=> {
+
+  var userName = req.body.userName;
+  var password = req.body.password;
+
+  if (userName === 'a' && password == 'a') {
+    var admin = {
+      userName: userName,
+      password: password
+    };
+    req.session.user = admin;
+    req.session.user.role = 'admin';
+
+    res.send(JSON.stringify({result: 0}));
+  } else {
+    res.send(JSON.stringify({result: -1, reason: '账户或者密码错误！~！'}))
+  }
+});
+
 router.post('/logout.do', (req, res)=> {
   delete req.session.user;
   res.send(JSON.stringify({
--- routes/users.js
+++ routes/users.js
@@ -6,24 +6,4 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/login.do', (req, res)=> {
-
-  var userName = req.body.userName;
-  var password = req.body.password;
-
-  if (userName === 'a' && password == 'a') {
-    var admin = {
-      userName: userName,
-      password: password
-    };
-    req.session.user = admin;
-    req.session.user.role = 'admin';
-
-    res.send(JSON.stringify({result: 0}));
-  } else {
-    res.send(JSON.stringify({result: -1, reason: '账户或者密码错误！~！'}))
-  }
-
-});
-
 module.exports = router;
