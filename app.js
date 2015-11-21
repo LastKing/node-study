dedef68bf080f9d4d5476ba3ed61018adcc1e5ff
李太新 <toonew@danwi.me>
2015-11-21 22:23:07 +0800

    替换log4j 任务支持

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var log4js = require('log4js');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -18,7 +19,8 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
+app.use(log4js.connectLogger(log4js.getLogger('express'), {level: log4js.levels.INFO}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
