07304a721b1722e545493983e758db20fbdcf313
李太新 <toonew@danwi.me>
2015-11-24 11:29:13 +0800

    邮件模块测试成功，提交，等待成为一个common 模块

--- common/email_helper.js
+++ common/email_helper.js
@@ -1,10 +1,10 @@
 /**
- * Created by Rain on 2015/11/23.
+ * Created by Rain on 2015/11/24.
  */
 var nodemailer = require('nodemailer');
 
 /**
- * 改写法 适合 0.7.1 及以下的版本
+ * 该写法 只适合 nodemailer以及 0.7.1 及以下的版本
  * 并且需要开启  SMTP 服务
  */
 var transporter = nodemailer.createTransport("SMTP", {
@@ -30,3 +30,7 @@ transporter.sendMail({
   }
   transporter.close(); // 如果没用，关闭连接池
 });
+
+module.exports = {
+  transport: transporter
+};
