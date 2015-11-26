b5f26fbe2d6841b6fa19f246e672f59edd1d19b0
李太新 <toonew@danwi.me>
2015-11-26 10:59:12 +0800

    注册验证。。

--- routes/font/auth.js
+++ routes/font/auth.js
@@ -1,4 +1,11 @@
 /**
+ * 所有认证相关的类型请求
+ *
+ * get  请求为  需要访问的页面
+ * post 请求为  为请求访问页面的对应的操作。
+ *
+ * 必须 register 为注册，get指向页面，post为真是注册的方法
+ *
  * Created by Rain on 2015/11/25.
  */
 var express = require('express');
@@ -15,6 +22,7 @@ router.get('/register.do', (req, res)=> {
 router.post('/register.do', (req, res)=> {
   var user = req.body;
 
+  res.send("test", JSON.stringify({"result": 0}));
 });
 
 
